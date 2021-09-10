c5305dde82ac1eccce384c92dd6bdc8ffae9ea5d
Gabriel <gabriel.ortiz@ninjarmm.com>
2021-09-09 21:57:40 -0500

    [gaortiz1] init project

---
+++ shoppingcart/src/test/kotlin/com/tul/shoppingcart/ShoppingcartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tul.shoppingcart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
